f47034a196b444d02aa0500c1049e7517fc0da06
codinggatsby <gabriellangley2017@gmail.com>
2022-11-14 10:03:29 -0500

    wokring

---
+++ courses/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const courseTableBody = document.getElementById("courseTableBody")
+
+function loadcourseTableBody() {
+    fetch = ("http://localhost:8081/api/courses");
+    
+    
+}
